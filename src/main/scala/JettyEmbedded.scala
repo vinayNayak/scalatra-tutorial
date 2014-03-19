b457af23c5f579d9a4bd8c887e427728b4ad9a53
vinay nayak <vinay.nayak@yahoo.com>
2014-03-19 23:07:58 +0000

    Working app with embedded jetty

--- src/main/scala/JettyEmbedded.scala
+++ src/main/scala/JettyEmbedded.scala
@@ -1,24 +1,17 @@
-import org.eclipse.jetty.server.nio.SelectChannelConnector
 import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.webapp.WebAppContext
+import org.scalatra.servlet.ScalatraListener
 
-object JettyEmbedded extends  App{
+object JettyEmbedded extends App {
 
-  val server = new Server()
-  val connector = new SelectChannelConnector()
-  connector.setPort(8080)
-  server.addConnector(connector)
-  val context: WebAppContext = new WebAppContext("src/main/webapp", "/")
-  context.setServer(server)
+  val server = new Server(8080)
+  val context = new WebAppContext()
+  context setContextPath "/"
+  context.setResourceBase("src/main/webapp")
+  context.addEventListener(new ScalatraListener)
+  context.addServlet(classOf[DefaultServlet], "/")
   server.setHandler(context)
-
-  try{
-    server.start()
-    server.join()
-  }catch {
-    case e: Exception =>
-      e.printStackTrace()
-      System.exit(1)
-  }
-
+  server.start
+  server.join
 }
