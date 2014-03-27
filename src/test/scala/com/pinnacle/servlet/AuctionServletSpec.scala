ceb8a3cdebdaa106bdd107210d960083a6ae00a0
vinay nayak <vinay.nayak@yahoo.com>
2014-03-27 00:38:49 +0000

    updated tests

--- src/test/scala/com/pinnacle/servlet/AuctionServletSpec.scala
+++ src/test/scala/com/pinnacle/servlet/AuctionServletSpec.scala
@@ -4,12 +4,31 @@ import org.scalatra.test.specs2.ScalatraSpec
 
 class AuctionServletSpec extends ScalatraSpec {
 
-  addServlet(classOf[AuctionServlet], "/auction")
+  addServlet(classOf[AuctionServlet], "/auction/*")
+
+  val ITEMS_URL = "/auction/items/123"
 
   def is =
     "Calling an unknown url on the API" ^
       "returns status 404" ! (statusCode("/unknown") == 404) ^
+      end ^
+      br ^
+      "Calling a GET on " + ITEMS_URL + " should" ^
+      "return status 200" ! (statusCode(ITEMS_URL) == 200) ^
       end
+
+  /* def is =
+     "Calling an unknown url on the API " ^
+       "returns status 404" ! statusResult("/unknown", 404) ^
+       end ^ p ^
+       "Calling a GET on " + ITEMS_URL + " should" ^
+       "return status 200" ! statusResult(ITEMS_URL, 200) ^
+       end
+
+   def statusResult(url: String, code: Int) =
+     get(url) {
+       status must_== code
+     }*/
 
   def statusCode(url: String): Int = {
     get(url) {
