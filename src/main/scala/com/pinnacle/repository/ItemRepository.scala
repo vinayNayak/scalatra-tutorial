c164875d5345d36f4e970c78c699283dbc13c83b
vinay nayak <vinay.nayak@yahoo.com>
2014-04-06 00:12:09 +0100

    findOne implementation with casbah

--- src/main/scala/com/pinnacle/repository/ItemRepository.scala
+++ src/main/scala/com/pinnacle/repository/ItemRepository.scala
@@ -1,12 +1,21 @@
 package com.pinnacle.repository
 
 import com.pinnacle.domain.Item
-import com.mongodb.casbah.MongoClient
+import com.mongodb.casbah.{MongoCollection, MongoClient}
 
 class ItemRepository(mongoClient: MongoClient = MongoClient()) {
+
+  implicit val db = mongoClient("auction")
 
   def get(id: Long): Option[Item] = id match {
     case 123 => Some(Item(id, "Monty Python and the search for the holy grail", 0.69, "GBP", "Must have item"))
     case _ => None
+  }
+
+  def findOne(): Option[Item] = {
+    val itemsColl = db("items")
+    val result: Option[MongoCollection#T] = itemsColl.findOne()
+    result.map(obj => Item(obj.get("_id").asInstanceOf[Double].toLong, obj.get("name").asInstanceOf[String],
+      obj.get("startPrice").asInstanceOf[Number], obj.get("currency").asInstanceOf[String], obj.get("description").asInstanceOf[String]))
   }
 }
--- src/test/scala/com/pinnacle/repository/ItemRepositorySpec.scala
+++ src/test/scala/com/pinnacle/repository/ItemRepositorySpec.scala
@@ -9,12 +9,17 @@ class ItemRepositorySpec extends UnitSpec {
 
   before {}
 
-  "Item Repository" should "return an item for id 123" in {
+  "Get By Id" should "return an item for id 123" in {
     val expected = Item(123, "Monty Python and the search for the holy grail", 0.69, "GBP", "Must have item")
     repo.get(123) should be(Some(expected))
   }
 
   it should "return None if the item is not present" in {
     repo.get(-1) should be === None
+  }
+
+  "Find One" should "return an item" in {
+    val expected = Item(123, "Monty Python and the search for the holy grail", 0.69, "GBP", "Must have item")
+    repo.findOne() should be === Some(expected)
   }
 }
